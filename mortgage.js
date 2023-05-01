9e1f0273097ed2c665a185ae8f274135a89daf1c
Zion Danielson <zionsdocs@gmail.com>
2023-05-01 15:58:42 -0400

    index.html

--- mortgage.js
+++ mortgage.js
@@ -1,16 +1,8 @@
-"use strict";
-
 //Variables: These are all of the elements you wish to interact
 // on your page:
 
-//Home Price textbox
-const homePrice        = document.getElementById("homePrice");
-//Down Payment textbox
-const downPayment      = document.getElementById("downPayment");
-//Loan Term textbox
-const loanTerm         = document.getElementById("loanTerm");
-//Interest Rate
-const interestRate     = document.getElementById("interestRate");
+
+
 
 
 window.onload = init;
@@ -23,21 +15,22 @@ window.onload = init;
 
 function init() {
 
-calculateMortgage.onclick
-}
-
-function calculateMortgage() {
-  
-    const principal = homePrice - downPayment;
-    const monthlyInterestRate = (interestRate / 100) / 12;
-    const numberOfPayments = loanTerm * 12;
-  
-    const x = Math.pow(1 + monthlyInterestRate, numberOfPayments);
-    const monthlyPayment = (principal*x*monthlyInterestRate)/(x-1);
+        const homePrice = parseFloat(document.getElementById("homePrice").value);
+        const downPayment = parseFloat(document.getElementById("downPayment").value);
+        const loanTerm = parseFloat(document.getElementById("loanTerm").value);
+        const interestRate = parseFloat(document.getElementById("interestRate").value);
+        const result = parseFloat(document.getElementById("result").value);
+        
+        const principal = homePrice - downPayment;
+        const monthlyInterestRate = (interestRate / 100) / 12;
+        const numberOfPayments = loanTerm * 12;
+        const numerator = monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments);
+        const denominator = Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1;
+        const monthlyPayment = principal * (numerator / denominator);
     
-    const resultElement = document.getElementById("result");
+        
+        result.value = "$" + monthlyPayment;
+        
+    }
 
-    resultElement.innerHTML = ${monthlyPayment.toFixed(2)};
-    
-  }
-  
+console.log("the index.js has finished executing..."); // Final end Message in the console
